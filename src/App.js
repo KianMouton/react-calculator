23df50a19a0f80febbfd1bc09800167486ff0040
KianMouton <kianton2004@gmail.com>
2024-06-30 14:10:27 +0200

    started working on css

--- src/App.js
+++ src/App.js
@@ -3,36 +3,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="calculator">
-      <div id="display">
-        0
-      </div>
-      <div className="cal-col">
-        <button id="clear">C</button>
-        <button id="divide">/</button>
-        <button id="multiply">*</button>
-      </div>
-      <div class="cal-col">
-        <button id="1">1</button>
-        <button id="2">2</button>
-        <button id="3">3</button>
-      </div>
-      <div className="cal-col">
-        <button id="4">4</button>
-        <button id="5">5</button>
-        <button id="6">6</button>
-      </div>
-      <div className="cal-col">
-        <button id="7">7</button>
-        <button id="8">8</button>
-        <button id="9">9</button>
-      </div>
-      <div className="functions">
-        <button id="subtract">-</button>
-        <button id="add">+</button>
-        <button id="equals">=</button>
+    <div className="App">
+      <div className="calculator">
+        <div id="display">
+          0
+        </div>
+        <div className="buttons">
+          <div className="cal-col">
+            <button id="clear">C</button>
+            <button id="divide">/</button>
+            <button id="multiply">*</button>
+          </div>
+          <div class="cal-col">
+            <button id="1">1</button>
+            <button id="2">2</button>
+            <button id="3">3</button>
+          </div>
+          <div className="cal-col">
+            <button id="4">4</button>
+            <button id="5">5</button>
+            <button id="6">6</button>
+          </div>
+          <div className="cal-col">
+            <button id="7">7</button>
+            <button id="8">8</button>
+            <button id="9">9</button>
+          </div>
+          <div className="functions">
+            <button id="subtract">-</button>
+            <button id="add">+</button>
+            <button id="equals">=</button>
+        </div>
+        </div>
       </div>
     </div>
+    
   );
 }
 
