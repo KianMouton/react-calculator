c8a283f5eed4f5028281874f1fbed3c0c3b11177
KianMouton <kianton2004@gmail.com>
2024-06-30 13:26:37 +0200

    added buttons

--- src/App.js
+++ src/App.js
@@ -3,8 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>hello world</p>
+    <div className="calculator">
+      <div id="display">
+        0
+      </div>
+      <div className="cal-col">
+        <button id="clear">C</button>
+        <button id="divide">/</button>
+        <button id="multiply">*</button>
+      </div>
+      <div class="cal-col">
+        <button id="1">1</button>
+        <button id="2">2</button>
+        <button id="3">3</button>
+      </div>
+      <div className="cal-col">
+        <button id="4">4</button>
+        <button id="5">5</button>
+        <button id="6">6</button>
+      </div>
+      <div className="cal-col">
+        <button id="7">7</button>
+        <button id="8">8</button>
+        <button id="9">9</button>
+      </div>
+      <div className="functions">
+        <button id="subtract">-</button>
+        <button id="add">+</button>
+        <button id="equals">=</button>
+      </div>
     </div>
   );
 }
