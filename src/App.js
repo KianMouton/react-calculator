aaa7701c80379d3062a459b3f177ca3c35f257c4
KianMouton <kianton2004@gmail.com>
2024-07-01 21:49:42 +0200

    some more css changes

--- src/App.js
+++ src/App.js
@@ -50,29 +50,29 @@ function App() {
         </div>
         <div className="buttons">
           <div className="cal-col">
+            <button onClick={handleNumber} id="zero">0</button>
             <button onClick={handleClear} id="clear">Clear</button>
             <button onClick={handleNumber} id="divide">/</button>
             <button onClick={handleNumber} id="multiply">*</button> 
           </div>
           <div class="cal-col">
-            <button onClick={handleNumber} id="zero">0</button>
             <button onClick={handleNumber} id="one">1</button>
             <button onClick={handleNumber} id="two">2</button>
             <button onClick={handleNumber} id="three">3</button>
             <button onClick={handleNumber} id="subtract">-</button>
+            <button onClick={handleNumber} id="add">+</button>
           </div>
           <div className="cal-col">
             <button onClick={handleNumber} id="four">4</button>
             <button onClick={handleNumber} id="five">5</button>
             <button onClick={handleNumber} id="six">6</button>
-            <button onClick={handleNumber} id="add">+</button>
-            <button onClick={equals} id="equals">=</button>
+            <button onClick={handleNumber} id="decimal">.</button>
           </div>
           <div className="cal-col">
             <button onClick={handleNumber} id="seven">7</button>
             <button onClick={handleNumber} id="eight">8</button>
             <button onClick={handleNumber} id="nine">9</button>
-            <button onClick={handleNumber} id="decimal">.</button>
+            <button onClick={equals} id="equals">=</button>
           </div>
         </div>
       </div>
