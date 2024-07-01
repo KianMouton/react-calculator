dbaeebd3b24f1e3f967cf5d8f5415a2bca9c74da
KianMouton <kianton2004@gmail.com>
2024-07-01 12:47:04 +0200

    fixed decimals not working

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 function App() {
 
   const [ input, setInput ] = useState("");
+  const [ previous, setPrevious ] = useState("");
 
   //clears text in the input
   const handleClear = () => {
     setInput("");
+    setPrevious("");
   }
 
   const handleNumber = (event) => {
@@ -21,9 +23,11 @@ function App() {
 
   const equals = () => {
     const output = document.getElementById("output");
-    const values = input.split("").join(" ");
+    const previousValue = document.getElementById("previous");
+    const values = input;
     const result = evaluate(values);
-    output.textContent += "=" + result;
+    output.textContent += "=";
+    previousValue.textContent = result;
   }
 
   return (
@@ -31,6 +35,7 @@ function App() {
       <div className="calculator">
         <div id="display">
           <p id="output">{input}</p>
+          <p id="previous">{previous}</p>
         </div>
         <div className="buttons">
           <div className="cal-col">
@@ -40,21 +45,22 @@ function App() {
             <button onClick={handleNumber} id="decimal">.</button>
           </div>
           <div class="cal-col">
-            <button onClick={handleNumber} id="1">1</button>
-            <button onClick={handleNumber} id="2">2</button>
-            <button onClick={handleNumber} id="3">3</button>
+            <button onClick={handleNumber} id="zero">0</button>
+            <button onClick={handleNumber} id="one">1</button>
+            <button onClick={handleNumber} id="two">2</button>
+            <button onClick={handleNumber} id="three">3</button>
             <button onClick={handleNumber} id="subtract">-</button>
           </div>
           <div className="cal-col">
-            <button onClick={handleNumber} id="4">4</button>
-            <button onClick={handleNumber} id="5">5</button>
-            <button onClick={handleNumber} id="6">6</button>
+            <button onClick={handleNumber} id="four">4</button>
+            <button onClick={handleNumber} id="five">5</button>
+            <button onClick={handleNumber} id="six">6</button>
             <button onClick={handleNumber} id="add">+</button>
           </div>
           <div className="cal-col">
-            <button onClick={handleNumber} id="7">7</button>
-            <button onClick={handleNumber} id="8">8</button>
-            <button onClick={handleNumber} id="9">9</button>
+            <button onClick={handleNumber} id="seven">7</button>
+            <button onClick={handleNumber} id="eight">8</button>
+            <button onClick={handleNumber} id="nine">9</button>
             <button onClick={equals} id="equals">=</button>
           </div>
           
